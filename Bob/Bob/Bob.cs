384dbeaf5619f88dce32b3ab5a895ab463a261c8
Andrii Pelekh <pelekhav@gmail.com>
2015-03-11 13:19:06 -0600

    Changes 3-11-15

--- Bob/Bob/Bob.cs
+++ Bob/Bob/Bob.cs
@@ -4,10 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("You are, what, like 15?".Contains("?"));
+        Bob bob = new Bob();
+        bob.Hey("ABCD!");
+
+        Console.ReadKey();
+    }
+
+}
+
 class Bob
 {
     public string Hey(string bobsWords)
     {
-        return string.Empty; // Replace this
+        List<string> words = new List<string>(bobsWords.Split(' ').ToList());
+        //for (int i = 0; i < words.Count; i++)
+        //{
+        //    if (words[i].Length > 1)
+        //    {
+        //        for (int j = 1; j < words[i].Length; j++)
+        //        {
+        //            if (words[i][j] >= 'A' && words[i][j] <= 'Z')
+        //            {
+        //                return "Whoa, chill out!";
+        //            }
+        //        }
+        //    }
+        //}
+
+        if (bobsWords.Trim() == string.Empty)
+        {
+            return "Fine. Be that way!";
+        }
+        else if (words.Where(x => x.Length > 1).Any(x => x.Skip(1).Any(y => y >= 'A' && y <= 'Z')) && !words.Any(x => x.ToLower() == "ok"))
+        {
+            return "Whoa, chill out!";
+        }
+        else if (bobsWords.Trim().Last() == '?' && !bobsWords.Trim().Contains("."))
+        {
+            return "Sure.";
+        }
+
+
+        return "Whatever."; // Replace this
     }
+
 }
